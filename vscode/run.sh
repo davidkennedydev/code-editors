1e4fa68dc385f2c15e211df41eb98dd70f619cec
David Kennedy S. Araujo <daviduser.guj@gmail.com>
2020-09-25 20:32:34 -0300

    Treat SE Linux blocking GUI sharing with docker container

--- vscode/run.sh
+++ vscode/run.sh
@@ -1,1 +1,7 @@
-docker run --rm -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --net host $(basename $PWD)
+# set SELINUX to permissive mode and enable X11 forwarding
+sudo setenforce 0 || true
+
+docker run --rm -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:Z --net host $(basename $PWD)
+
+# set SELINUX to enforcing
+sudo setenforce 1 || true
